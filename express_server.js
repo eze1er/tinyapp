cfbc7d4113893621b3b0a2ca957a03d54dd2216a
eze1er <papaezechiel@gmail.com>
2021-12-07 06:46:38 +0000

    Render the shortUrl and the longUrl

--- express_server.js
+++ express_server.js
@@ -39,6 +39,10 @@ app.get("/hello", (req, res) => {
   const templateVars = { greeting: 'Hello World!' };
   res.render("hello_world", templateVars);
 });
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL]};
+  res.render("urls_show", templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
