72ddd6083ef847514aa4d189b0d577e24200a92c
eze1er <papaezechiel@gmail.com>
2021-12-07 05:27:41 +0000

    Modification of express_server.js and views/url_index

--- express_server.js
+++ express_server.js
@@ -30,6 +30,16 @@ app.get("/set", (req, res) => {
   res.send(`a = ${a}`);
  });
 
+ app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/hello", (req, res) => {
+  const templateVars = { greeting: 'Hello World!' };
+  res.render("hello_world", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
