bd40f21684f05e862d7869366bfcb858d48aa37a
eze1er <papaezechiel@gmail.com>
2021-12-07 20:09:45 +0000

    Commit when we have form on

--- express_server.js
+++ express_server.js
@@ -39,6 +39,11 @@ app.get("/hello", (req, res) => {
   const templateVars = { greeting: 'Hello World!' };
   res.render("hello_world", templateVars);
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
